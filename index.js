b56263c4ac6b99bf39d52fb3a49e90c7787cb994
slin12 <s.lin12@gmail.com>
2017-11-18 14:59:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+function scuberGreetingForFeet(number) {
+    if (number <= 400) {
+      return "This one is on me!";
+    } else if (number > 2000 && number < 2500) {
+      return 'I will gladly take your thirty bucks.'
+    } else if (number > 2500) {
+      return 'No can do.'
+    }
+}
+
+function ternaryCheckCity(city) {
+  answer = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+  return answer;
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
+}
