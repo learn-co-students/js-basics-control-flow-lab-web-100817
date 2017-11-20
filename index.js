d038f0fd6d9a228ef748b6c324b3e8796fc9e472
Fabiano Cunha <cunhasb@aol.com>
2017-11-20 16:37:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+function scuberGreetingForFeet(feet){
+switch(true) {
+  case feet >2500:
+    return 'No can do.'
+    break;
+  case feet >=2000:
+    return 'I will gladly take your thirty bucks.'
+    break;
+  case feet <= 400:
+    return 'This one is on me!';
+    break;
+};}
+
+function ternaryCheckCity(city){
+    return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';;
+}
+
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+        return 'Thank you.';
+        break;
+    default:
+      return 'Bye.';
+
+      break;
+  }
+}
