e990a4304616d4f733f2e5a20fcc51edf4148189
Dakota Lillie <dakota.lillie@icloud.com>
2017-11-21 14:20:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(value) {
+  if (value <= 400) {
+    return 'This one is on me!'
+  } else if (value > 2000 && value <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  } else if (value > 2500) {
+    return 'No can do.'
+  }
+}
+
+function ternaryCheckCity(city) {
+  let answer;
+  city === 'NYC' ? answer = 'Ok, sounds good.' : answer = 'No go.';
+  return answer;
+}
+
+function switchOnCharmFromTip(tip) {
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.'
+      break;
+    default:
+      response = 'Bye.'
+      break;
+  }
+  return response;
+}
